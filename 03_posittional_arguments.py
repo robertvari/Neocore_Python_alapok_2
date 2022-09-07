c626f9ff3f59621c0e1e4e4a1fa82b7be70de9b2
Robert Var <mail.robertvari@gmail.com>
2022-09-07 14:31:16 +0200

    update

---
+++ 03_posittional_arguments.py
@@ -0,0 +1,8 @@
+# required posittional arguments/parameters
+def hello(email, address, name):
+    print(f"Hello {name}")
+    print(f"Email: {email}")
+    print(f"Address: {address}")
+
+# keyword parameters
+hello(name="Csaba", address="Budapest", email="robert@gmail.com")
