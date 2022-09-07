8d108f73dec30c37be0f0aa3dfda0ca8b705ca10
Robert Var <mail.robertvari@gmail.com>
2022-09-07 15:37:07 +0200

    Update

--- 09_lambdas.py
+++ 09_lambdas.py
@@ -7,3 +7,13 @@ divide_numbers = lambda a, b: a/b
 # print(divide_numbers(10, 4))
 
 name_list = ["Vári Róbert", "Kiss Elemér", "Nagy Adrienn", "Tóth Barna", "Kiss Elemér Aladár"]
+print(sorted(name_list))
+print(sorted(name_list, key=lambda name: name.split()[-1]))
+
+
+movie_list = {
+    1: {"name": "The Princess", "rating": 63},
+    2: {"name": "Spider-Man: No Way Home", "rating": 81},
+    3: {"name": "Morbius", "rating": 64},
+    4: {"name": "The Northman", "rating": 72},
+}
