e8e9ea0e427e55325709ed3258b0ba344d4eeeb9
Robert Var <mail.robertvari@gmail.com>
2022-09-07 16:00:04 +0200

    update

---
+++ 11_import_local_library.py
@@ -0,0 +1,5 @@
+from my_utils import math_functions as math
+
+print(math.add_numbers(10, 4))
+print(math.multiply_numbers(10, 4))
+print(math.divide_numbers(10, 4))
---
+++ my_utils/math_functions.py
@@ -0,0 +1,10 @@
+# ONLY FUNCTION DEFINITIONS
+
+def add_numbers(a: int, b: int):
+    return a + b
+
+def multiply_numbers(a: int, b: int):
+    return a * b
+
+def divide_numbers(a: int, b: int):
+    return a / b
