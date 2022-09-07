b9e69bcf232c0792a870094d8c0908d789f297a2
Robert Var <mail.robertvari@gmail.com>
2022-09-07 15:52:21 +0200

    update

---
+++ 10_import_standard_libraries.py
@@ -0,0 +1,6 @@
+import os  # import function library
+from os import listdir  # select a function from the library
+from os import listdir as ls  # rename a function (aliassing)
+import time
+import random
+import os, time, random
